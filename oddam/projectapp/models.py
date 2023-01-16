6aea81b94f3ab8da2af02a1cfaabc0898beb4072
mateuszezil <mwasilewski92@gmail.com>
2023-01-16 19:57:43 +0100

    Feature 1.1 and 1.2 added

--- oddam/projectapp/models.py
+++ oddam/projectapp/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Institution(models.Model):
+    TYPE_OF_INSTITUTION_CHOICES = [
+        ('Fundacja', 'Fundacja'),
+        ('Organizacja Pozarządowa', 'Organizacja Pozarządowa'),
+        ('Zbiórka Lokalna', 'Zbiórka Lokalna')
+    ]
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    type = models.CharField(
+        max_length=40,
+        choices=TYPE_OF_INSTITUTION_CHOICES,
+        default='Fundacja',
+    )
+    categories = models.ManyToManyField(Category)
