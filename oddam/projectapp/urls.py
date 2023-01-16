d6b59b7fabe7b1c0d48347d5b3b3b64d86cbbc30
mateuszezil <mwasilewski92@gmail.com>
2023-01-16 14:02:04 +0100

    future 0.1

---
+++ oddam/projectapp/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'projectapp'
+
+urlpatterns = [
+    path('', views.LandingPage.as_view(), name='landingpage'),
+    path('add', views.AddDonation.as_view(), name='add'),
+    path('login', views.Login.as_view(), name='login'),
+    path('register', views.Register.as_view(), name='register'),
+
+
+]
