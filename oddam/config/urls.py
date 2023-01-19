ad44b190663ec8691bfbd590343721a1d9711118
mateuszezil <mwasilewski92@gmail.com>
2023-01-19 22:44:57 +0100

    Feature 4 Comleted

--- oddam/config/urls.py
+++ oddam/config/urls.py
@@ -18,5 +18,6 @@ from django.urls import path
 from django.urls import include
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("accounts/", include("django.contrib.auth.urls")),
     path('', include('projectapp.urls')),
 ]
