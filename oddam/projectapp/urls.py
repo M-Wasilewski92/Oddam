a7663d14a868d99fed910e8f9ba623748f1e5047
mateuszezil <mwasilewski92@gmail.com>
2023-01-25 11:39:50 +0100

    feature 9

--- oddam/projectapp/urls.py
+++ oddam/projectapp/urls.py
@@ -9,7 +9,8 @@ urlpatterns = [
     path('add/', views.AddDonation.as_view(), name='add'),
     path('login/', views.Login.as_view(), name='login'),
     path('register/', views.Register.as_view(), name='register'),
-    path('donation/', views.SucessDonation.as_view(), name='donation'),
+    path('donation/', views.SuccessDonation.as_view(), name='donation'),
+    path('profile/', views.UserProfile.as_view(), name='profile'),
 
 
 ]
