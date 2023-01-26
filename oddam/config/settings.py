3ee5ea87e2aaf07304353733a0bbdfbed7ebfccf
mateuszezil <mwasilewski92@gmail.com>
2023-01-26 23:48:40 +0100

    unsafe storage fix

--- oddam/config/settings.py
+++ oddam/config/settings.py
@@ -132,12 +132,4 @@ LOGIN_URL = "projectapp:login"
 LOGOUT_REDIRECT_URL = "projectapp:landingpage"
 
 # Emailing settings
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_FROM = 'Mwasilewski92@gmail.com'
-EMAIL_HOST_USER = 'Mwasilewski92@gmail.com'
-EMAIL_HOST_PASSWORD =
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
 
-PASSWORD_RESET_TIMEOUT = 14400
